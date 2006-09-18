cc94f36512026d300fb828d7b42330ee9de61f12
cwen@iupui.edu <cwen@iupui.edu@66ffb92e-73f9-0310-93c1-f5514f145a0a>
2006-09-18 19:45:19 +0000

    maxResultSetNumber@org.sakaiproject.component.privacy.PrivacyManagerImpl => maximum result set size setting for db query (default is 1000)
    git-svn-id: https://source.sakaiproject.org/svn/privacy/trunk@14877 66ffb92e-73f9-0310-93c1-f5514f145a0a

--- privacy-comp-impl/src/java/org/sakaiproject/component/privacy/PrivacyManagerImpl.java
+++ privacy-comp-impl/src/java/org/sakaiproject/component/privacy/PrivacyManagerImpl.java
@@ -2,6 +2,7 @@ package org.sakaiproject.component.privacy;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 import java.util.Iterator;
@@ -17,6 +18,10 @@ import org.hibernate.Session;
 import org.sakaiproject.api.privacy.PrivacyManager;
 import org.sakaiproject.api.privacy.PrivacyRecord;
 import org.sakaiproject.hbm.privacy.PrivacyRecordImpl;
+import org.sakaiproject.user.api.User;
+import org.sakaiproject.user.cover.UserDirectoryService;
+import org.sakaiproject.authz.cover.AuthzGroupService;
+import org.sakaiproject.authz.api.AuthzGroup;
 
 import org.springframework.orm.hibernate3.HibernateCallback;
 import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
@@ -30,6 +35,8 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
 	private static final String QUERY_BY_DISABLED_USERID_CONTEXTID = "findDisabledPrivacyUserIdContextIdType";
 	private static final String QUERY_BY_CONTEXT_VIEWABLE_TYPE = "finalPrivacyByContextViewableType";
 	private static final String QUERY_BY_CONTEXT__TYPE = "finalPrivacyByContextType";
+	private static final String QUERY_BY_CONTEXT__TYPE_IDLIST = "finalPrivacyByContextTypeAndUserIds";
+	private static final String QUERY_BY_CONTEXT_VIEWABLE_TYPE_IDLIST = "finalPrivacyByContextViewableTypeUserList";
 	private static final String CONTEXT_ID = "contextId";
 	private static final String USER_ID = "userId";
 	private static final String RECORD_TYPE = "recordType";
@@ -38,6 +45,7 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
 	protected boolean defaultViewable = true;
 	protected Boolean overrideViewable = null;
 	protected boolean userRecordHasPrecedence = true;
+	protected int maxResultSetNumber = 1000;
 
 	public Set findViewable(String contextId, Set userIds)
 	{
@@ -67,25 +75,68 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
 		return returnSet;
 	}
 
-	public Set getViewableState(String contextId, Boolean value, String recordType)
+	public Set getViewableState (String contextId, Boolean value, String recordType)
 	{
   	if(contextId == null || value == null || recordType == null)
   	{
       throw new IllegalArgumentException("Null Argument in getViewableState");
   	}
+  	
+  	try
+  	{
+  		AuthzGroup realm = AuthzGroupService.getAuthzGroup(contextId);
+  		List users = new ArrayList();
+  		users.addAll(UserDirectoryService.getUsers(realm.getUsers()));
+  		List siteUserIds = new ArrayList();
+  		for(int i=0; i < users.size(); i++)
+  			siteUserIds.add(((User)users.get(i)).getId());
 
-		List returnedList = getViewableStateList(contextId, value, recordType);
-		if(returnedList != null)
-		{
-			Set returnSet = new HashSet();
-			for(int i=0; i<returnedList.size(); i++)
-			{
-				returnSet.add(((PrivacyRecord)returnedList.get(i)).getUserId());
-			}
-			return returnSet;
-		}
-		else
-			return null;
+  		//List returnedList = getViewableStateList(contextId, value, recordType);
+  		List returnedList = new ArrayList();
+  		List pieceList = new ArrayList();
+  		List resultPieceList = new ArrayList();
+  		for(int i=0; i <= (int)(siteUserIds.size() / maxResultSetNumber); i++)
+  		{
+  			pieceList.clear();
+  			if(i == (int)(siteUserIds.size() / maxResultSetNumber))
+  			{
+  				for(int j=0; j<(siteUserIds.size() % maxResultSetNumber); j++)
+  				{
+  					pieceList.add(siteUserIds.get(j + ((int)i*maxResultSetNumber)));
+  				}
+  			}
+  			else
+  			{
+  				for(int j=0; j<maxResultSetNumber; j++)
+  				{
+  					pieceList.add(siteUserIds.get(j + ((int)i*maxResultSetNumber)));
+  				}
+  			}
+  			
+  			if(pieceList.size() > 0)
+  			{
+  				resultPieceList = getViewableStateList(contextId, value, recordType, pieceList);
+  				for(int j=0; j<resultPieceList.size(); j++)
+  					returnedList.add(resultPieceList.get(j));
+  			}
+  		}
+  		
+  		if(returnedList != null)
+  		{
+  			Set returnSet = new HashSet();
+  			for(int i=0; i<returnedList.size(); i++)
+  			{
+  				returnSet.add(((PrivacyRecord)returnedList.get(i)).getUserId());
+  			}
+  			return returnSet;
+  		}
+  		else
+  			return null;
+  	}
+  	catch(org.sakaiproject.authz.api.GroupNotDefinedException gnde)
+  	{
+  		return null;
+  	}
 	}
 
 	public Map getViewableState(String contextId, String recordType)
@@ -95,19 +146,62 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
       throw new IllegalArgumentException("Null Argument in getViewableState");
   	}
 
-  	List returnedList = getPrivacyByContextAndType(contextId, recordType);
-  	if(returnedList != null)
+  	try
   	{
-  		HashMap returnMap = new HashMap(); 
-			PrivacyRecord pr;
-  		for(int i=0; i<returnedList.size(); i++)
+  		AuthzGroup realm = AuthzGroupService.getAuthzGroup(contextId);
+  		List users = new ArrayList();
+  		users.addAll(UserDirectoryService.getUsers(realm.getUsers()));
+  		List siteUserIds = new ArrayList();
+  		for(int i=0; i < users.size(); i++)
+  			siteUserIds.add(((User)users.get(i)).getId());
+
+  		//List returnedList = getPrivacyByContextAndType(contextId, recordType);
+  		List returnedList = new ArrayList();
+  		List pieceList = new ArrayList();
+  		List resultPieceList = new ArrayList();
+  		for(int i=0; i <= (int)(siteUserIds.size() / maxResultSetNumber); i++)
   		{
-  			pr = (PrivacyRecord)returnedList.get(i);
-  			returnMap.put(pr.getUserId(), new Boolean(pr.getViewable()));
+  			pieceList.clear();
+  			if(i == (int)(siteUserIds.size() / maxResultSetNumber))
+  			{
+  				for(int j=0; j<(siteUserIds.size() % maxResultSetNumber); j++)
+  				{
+  					pieceList.add(siteUserIds.get(j + ((int)i*maxResultSetNumber)));
+  				}
+  			}
+  			else
+  			{
+  				for(int j=0; j<maxResultSetNumber; j++)
+  				{
+  					pieceList.add(siteUserIds.get(j + ((int)i*maxResultSetNumber)));
+  				}
+  			}
+
+  			if(pieceList.size() > 0)
+  			{
+  				resultPieceList = getPrivacyByContextAndTypeAndUserIds(contextId, recordType, pieceList);
+  				for(int j=0; j<resultPieceList.size(); j++)
+  					returnedList.add(resultPieceList.get(j));
+  			}
   		}
-  		return returnMap;
+
+  		if(returnedList != null)
+  		{
+  			HashMap returnMap = new HashMap(); 
+  			PrivacyRecord pr;
+  			for(int i=0; i<returnedList.size(); i++)
+  			{
+  				pr = (PrivacyRecord)returnedList.get(i);
+  				returnMap.put(pr.getUserId(), new Boolean(pr.getViewable()));
+  			}
+  			return returnMap;
+  		}
+  		return null;
   	}
-		return null;
+  	catch(org.sakaiproject.authz.api.GroupNotDefinedException gnde)
+  	{
+		  return null;
+  	}
 	}
 
 	public boolean isViewable(String contextId, String userId)
@@ -267,6 +361,30 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
     return (List) getHibernateTemplate().execute(hcb); 
   }
   
+  private List getViewableStateList(final String contextId, final Boolean viewable, final String recordType, final List userIds)
+  {
+  	if(contextId == null || viewable == null || recordType == null || userIds == null)
+  	{
+      throw new IllegalArgumentException("Null Argument in getViewableStateList");
+  	}
+  	
+    HibernateCallback hcb = new HibernateCallback()
+    {
+      public Object doInHibernate(Session session) throws HibernateException,
+          SQLException
+      {
+        Query q = session.getNamedQuery(QUERY_BY_CONTEXT_VIEWABLE_TYPE_IDLIST);
+        q.setParameter("contextId", contextId, Hibernate.STRING);
+        q.setParameter("viewable", viewable, Hibernate.BOOLEAN);
+        q.setParameter("recordType", recordType, Hibernate.STRING);
+        q.setParameterList("userIds", userIds);
+        return q.list();
+      }
+    };
+
+    return (List) getHibernateTemplate().execute(hcb); 
+  }
+  
   private List getPrivacyByContextAndType(final String contextId, final String recordType)
   {
   	if(contextId == null || recordType == null)
@@ -289,6 +407,29 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
   	return (List) getHibernateTemplate().executeFind(hcb);
   }
   
+  private List getPrivacyByContextAndTypeAndUserIds(final String contextId, final String recordType, final List userIds)
+  {
+  	if(contextId == null || recordType == null || userIds == null)
+  	{
+  		throw new IllegalArgumentException("Null Argument in getPrivacyByContextAndTypeAndUserIds");
+  	}
+  	
+  	HibernateCallback hcb = new HibernateCallback()
+  	{
+  		public Object doInHibernate(Session session) throws HibernateException,
+  		    SQLException
+  		{
+        Query q = session.getNamedQuery(QUERY_BY_CONTEXT__TYPE_IDLIST);
+        q.setParameter("contextId", contextId, Hibernate.STRING);
+        q.setParameter("recordType", recordType, Hibernate.STRING);
+        q.setParameterList("userIds", userIds);        
+        return q.list();
+  		}
+  	};
+  	
+  	return (List) getHibernateTemplate().executeFind(hcb);
+  }
+
   private void savePrivacyRecord(PrivacyRecord privacy)
   {
   	getHibernateTemplate().saveOrUpdate(privacy);
@@ -367,5 +508,16 @@ public class PrivacyManagerImpl extends HibernateDaoSupport implements PrivacyMa
 	public void setUserRecordHasPrecedence(boolean userRecordHasPrecedence)
 	{
 		this.userRecordHasPrecedence = userRecordHasPrecedence;
+	}
+	
+	/**
+	 * Set maximum result set number for database query, defulat is 1000.
+	 * 
+	 * @param maxResultSetNumber
+	 *          the maxResultSetNumber to set
+	 */
+	public void setMaxResultSetNumber(int maxResultSetNumber)
+	{
+		this.maxResultSetNumber = maxResultSetNumber;
 	}
 }
